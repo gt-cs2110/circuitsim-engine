f6eab62a2a7d42d2bcaee9b6b89acb2d8e04c691
Daniel Huang <99danielh@gmail.com>
2025-11-01 13:20:33 -0400

    split node.rs into categories (#6)
    * split node.rs into categories
    
    * Fix import of BitArray
    
    * Add doc comment to `floating_ports` helper
    
    ---------
    
    Co-authored-by: Henry Bui <henry.h.bui05@gmail.com>

---
+++ src/node/gates.rs
@@ -0,0 +1,534 @@
+use crate::bitarray::{BitArray, BitState, bitarr};
+use crate::node::{Component, PortProperties, PortType, PortUpdate, port_list};
+
+/// Minimum number of inputs for multi-input logic gates.
+pub const MIN_GATE_INPUTS: u8 = 2;
+/// Maximum number of inputs for multi-input logic gates.
+pub const MAX_GATE_INPUTS: u8 = 64;
+
+macro_rules! gates {
+    ($($(#[$m:meta])? $Id:ident: $f:expr, $invert:literal),*$(,)?) => {
+        $(
+            $(#[$m])?
+            #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+            pub struct $Id {
+                bitsize: u8,
+                n_inputs: u8
+            }
+            impl $Id {
+                /// Creates a new instance of the gate with specified bitsize and number of inputs.
+                pub fn new(bitsize: u8, n_inputs: u8) -> Self {
+                    Self {
+                        bitsize: bitsize.clamp(BitArray::MIN_BITSIZE, BitArray::MAX_BITSIZE),
+                        n_inputs: n_inputs.clamp(MIN_GATE_INPUTS, MAX_GATE_INPUTS)
+                    }
+                }
+            }
+            impl Component for $Id {
+                fn ports(&self) -> Vec<PortProperties> {
+                    port_list(&[
+                        // inputs
+                        (PortProperties { ty: PortType::Input, bitsize: self.bitsize }, self.n_inputs),
+                        // outputs
+                        (PortProperties { ty: PortType::Output, bitsize: self.bitsize }, 1),
+                    ])
+                }
+                fn run_inner(&self, _old_ports: &[BitArray], new_ports: &[BitArray]) -> Vec<PortUpdate> {
+                    let value = new_ports[..usize::from(self.n_inputs)].iter()
+                        .cloned()
+                        .reduce($f)
+                        .unwrap_or_else(|| bitarr![X; self.bitsize]);
+    
+                    vec![PortUpdate {
+                        index: usize::from(self.n_inputs),
+                        value: if $invert { !value } else { value }
+                    }]
+                }
+            }
+        )*
+    }
+}
+
+gates! {
+    /// An AND gate component.
+    And:  |a, b| a & b, false,
+    /// An OR gate component.
+    Or:   |a, b| a | b, false,
+    /// An XOR gate component.
+    Xor:  |a, b| a ^ b, false,
+    /// A NAND gate component.
+    Nand: |a, b| a & b, true,
+    /// A NOR gate component.
+    Nor:  |a, b| a | b, true,
+    /// A XNOR gate component.
+    Xnor: |a, b| a ^ b, true,
+}
+
+/// A NOT gate component.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Not {
+    bitsize: u8
+}
+impl Not {
+    /// Creates a new instance of the NOT gate with specified bitsize.
+    pub fn new(bitsize: u8) -> Self {
+        Self {
+            bitsize: bitsize.clamp(BitArray::MIN_BITSIZE, BitArray::MAX_BITSIZE)
+        }
+    }
+}
+impl Component for Not {
+    fn ports(&self) -> Vec<PortProperties> {
+        port_list(&[
+            // input
+            (PortProperties { ty: PortType::Input, bitsize: self.bitsize }, 1),
+            // output
+            (PortProperties { ty: PortType::Output, bitsize: self.bitsize }, 1),
+        ])
+    }
+
+    fn run_inner(&self, _old_ports: &[BitArray], new_ports: &[BitArray]) -> Vec<PortUpdate> {
+        vec![PortUpdate { index: 1, value: !new_ports[0] }]
+    }
+}
+
+
+/// A tri-state buffer component.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct TriState {
+    bitsize: u8
+}
+impl TriState {
+    /// Creates a new instance of the tri-state buffer with specified bitsize.
+    pub fn new(bitsize: u8) -> Self {
+        Self {
+            bitsize: bitsize.clamp(BitArray::MIN_BITSIZE, BitArray::MAX_BITSIZE)
+        }
+    }
+}
+impl Component for TriState {
+    fn ports(&self) -> Vec<PortProperties> {
+        port_list(&[
+            // selector
+            (PortProperties { ty: PortType::Input, bitsize: 1 }, 1),
+            // input
+            (PortProperties { ty: PortType::Input, bitsize: self.bitsize }, 1),
+            // output
+            (PortProperties { ty: PortType::Output, bitsize: self.bitsize }, 1),
+        ])
+    }
+
+    fn run_inner(&self, _old_ports: &[BitArray], new_ports: &[BitArray]) -> Vec<PortUpdate> {
+        let gate = new_ports[0].index(0);
+        let result = match gate {
+            BitState::High => new_ports[1],
+            BitState::Low | BitState::Imped => bitarr![Z; self.bitsize],
+            BitState::Unk => bitarr![X; self.bitsize],
+        };
+        vec![PortUpdate { index: 2, value: result }]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_and_gate() {
+        let gate = And::new(1, 2);
+        let in_a = bitarr![0];
+        let in_b = bitarr![1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        // Checks if we have only one update. Should be 1 for logic gates
+        // Checks if port 2, the output port for two input gates was updated
+        // 1 & 0 = 0;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![0] }],
+            "Expected a single update with index=2 and value=0 (1 & 0 = 0)"
+        );
+    }
+
+    #[test]
+    fn test_and_gate_multi_bit() {
+        let gate = And::new(4, 2);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 0];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        // 1011 & 1100 = 1000;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![1, 0, 0, 0] }],
+            "Expected a single update with index=2 and value=1000 (1011 & 1100 = 1000)"
+        );
+    }
+
+    #[test]
+    fn test_and_gate_3input_4bit() {
+        let gate = And::new(4, 3); // 3 inputs, 4-bit each
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 0];
+        let in_c = bitarr![1, 1, 1, 0];
+
+        let updates = gate.run(&[], &[in_a, in_b, in_c]);
+
+        // 1011 & 1100 & 1110 = 1000;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 3, value: bitarr![1, 0, 0, 0] }],
+            "Expected a single update with index=3 and value=1000 (1011 & 1100 & 1110 = 1000)"
+        );
+    }
+
+    #[test]
+    fn test_or_gate() {
+        let gate = Or::new(1, 2);
+        let in_a = bitarr![0];
+        let in_b = bitarr![1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        // 1 | 0 = 1;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![1] }],
+            "Expected a single update with index=2 and value=1 (1 | 0 = 1)"
+        );
+    }
+
+    #[test]
+    fn test_or_gate_multi_bit() {
+        let gate = Or::new(4, 2);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 0];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        // 1011 | 1100 = 1111;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![1, 1, 1, 1] }],
+            "Expected a single update with index=2 and value=1111 (1011 | 1100 = 1111)"
+        );
+    }
+
+    #[test]
+    fn test_or_gate_3input_4bit() {
+        let gate = Or::new(4, 3);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 0];
+        let in_c = bitarr![0, 1, 1, 0];
+
+        let updates = gate.run(&[], &[in_a, in_b, in_c]);
+
+        // 1011 | 1100 | 0110 = 1111;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 3, value: bitarr![1, 1, 1, 1] }],
+            "Expected a single update with index=3 and value=1111 (1011 | 1100 | 0110 = 1111)"
+        );
+    }
+
+    #[test]
+    fn test_xor_gate() {
+        let gate = Xor::new(1, 2);
+        let in_a = bitarr![0];
+        let in_b = bitarr![1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        // 1 ^ 0 = 1;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![1] }],
+            "Expected a single update with index=2 and value=1 (1 ^ 0 = 1)"
+        );
+    }
+
+    #[test]
+    fn test_xor_gate_multi_bit() {
+        let gate = Xor::new(4, 2);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        // 1011 ^ 1101 = 0110;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![0, 1, 1, 0] }],
+            "Expected a single update with index=2 and value=0110 (1011 ^ 1101 = 0110)"
+        );
+    }
+
+    #[test]
+    fn test_xor_gate_3input_4bit() {
+        let gate = Xor::new(4, 3);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 1];
+        let in_c = bitarr![0, 1, 1, 0];
+
+        let updates = gate.run(&[], &[in_a, in_b, in_c]);
+
+        // 1011 ^ 1101 ^ 0110 = 0000;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 3, value: bitarr![0, 0, 0, 0] }],
+            "Expected a single update with index=3 and value=0000 (1011 ^ 1101 ^ 0110 = 0000)"
+        );
+    }
+
+    #[test]
+    fn test_nand_gate() {
+        let gate = Nand::new(1, 2);
+        let in_a = bitarr![0];
+        let in_b = bitarr![1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![1] }],
+            "Expected a single update with index=2 and value=1 (!(1 & 0) = 1)"
+        );
+    }
+
+    #[test]
+    fn test_nand_gate_multi_bit() {
+        let gate = Nand::new(4, 2);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![0, 1, 1, 0] }],
+            "Expected a single update with index=2 and value=0110 (!(1011 & 1101) = 0110)"
+        );
+    }
+
+    #[test]
+    fn test_nand_gate_3input_4bit() {
+        let gate = Nand::new(4, 3);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 1];
+        let in_c = bitarr![1, 1, 1, 0];
+
+        let updates = gate.run(&[], &[in_a, in_b, in_c]);
+
+        // !(1011 & 1101 & 1110) = 0111;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 3, value: bitarr![0, 1, 1, 1] }],
+            "Expected a single update with index=3 and value=0111 (!(1011 & 1101 & 1110) = 0111)"
+        );
+    }
+
+    #[test]
+    fn test_nor_gate() {
+        let gate = Nor::new(1, 2);
+        let in_a = bitarr![0];
+        let in_b = bitarr![1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![0] }],
+            "Expected a single update with index=2 and value=0 (!(1 | 0) = 0)"
+        );
+    }
+
+    #[test]
+    fn test_nor_gate_multi_bit() {
+        let gate = Nor::new(4, 2);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![0, 0, 0, 0] }],
+            "Expected a single update with index=2 and value=0000 (!(1011 | 1101) = 0000)"
+        );
+    }
+
+    #[test]
+    fn test_nor_gate_3input_4bit() {
+        let gate = Nor::new(4, 3);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 1];
+        let in_c = bitarr![0, 1, 1, 0];
+
+        let updates = gate.run(&[], &[in_a, in_b, in_c]);
+
+        // !(1011 | 1101 | 0110) = 0000;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 3, value: bitarr![0, 0, 0, 0] }],
+            "Expected a single update with index=3 and value=0000 (!(1011 | 1101 | 0110) = 0000)"
+        );
+    }
+
+    #[test]
+    fn test_xnor_gate() {
+        let gate = Xnor::new(1, 2);
+        let in_a = bitarr![0];
+        let in_b = bitarr![1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![0] }],
+            "Expected a single update with index=2 and value=0 (!(1 ^ 0) = 0)"
+        );
+    }
+
+    #[test]
+    fn test_xnor_gate_multi_bit() {
+        let gate = Xnor::new(4, 2);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 1];
+
+        let updates = gate.run(&[], &[in_a, in_b]);
+
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 2, value: bitarr![1, 0, 0, 1] }],
+            "Expected a single update with index=2 and value=1001 (!(1011 ^ 1101) = 1001)"
+        );
+    }
+
+    #[test]
+    fn test_xnor_gate_3input_4bit() {
+        let gate = Xnor::new(4, 3);
+        let in_a = bitarr![1, 0, 1, 1];
+        let in_b = bitarr![1, 1, 0, 1];
+        let in_c = bitarr![0, 1, 1, 0];
+
+        let updates = gate.run(&[], &[in_a, in_b, in_c]);
+
+        // !(1011 ^ 1101 ^ 0110) = 1111;
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 3, value: bitarr![1, 1, 1, 1] }],
+            "Expected a single update with index=3 and value=1111 (!(1011 ^ 1101 ^ 0110) = 1111)"
+        );
+    }
+
+    #[test]
+    fn test_not_gate() {
+        let gate = Not::new(1);
+        let in_a = bitarr![0];
+
+        let updates = gate.run(&[], &[in_a]);
+
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 1, value: bitarr![1] }],
+            "Expected a single update with index=1 and value=1 (!0 = 1)"
+        );
+    }
+
+    #[test]
+    fn test_not_gate_multi_bit() {
+        let gate = Not::new(4);
+        let in_a = bitarr![1, 0, 1, 1];
+
+        let updates = gate.run(&[], &[in_a]);
+
+        assert_eq!(
+            updates,
+            vec![PortUpdate { index: 1, value: bitarr![0, 1, 0, 0] }],
+            "Expected a single update with index=1 and value=0100 (!1011 = 0100)"
+        );
+    }
+
+    mod input_validation {
+        use super::*;
+
+        #[test]
+        #[should_panic]
+        fn input_validate_and() {
+            let gate = And::new(4, 2);
+            // Should fail input validation
+            let bad_in = bitarr![1, 1, 1];
+            let good_in = bitarr![1, 0, 1, 0];
+            let _ = gate.run(&[], &[bad_in, good_in]);
+        } 
+
+        #[test]
+        #[should_panic]
+        fn input_validate_or() {
+            let gate = Or::new(4, 2);
+            // Should fail input validation
+            let bad_in = bitarr![1, 1, 1];
+            let good_in = bitarr![1, 0, 1, 0];
+            let _ = gate.run(&[], &[bad_in, good_in]);
+        } 
+
+        #[test]
+        #[should_panic]
+        fn input_validate_xor() {
+            let gate = Xor::new(4, 2);
+            // Should fail input validation
+            let bad_in = bitarr![1, 1, 1];
+            let good_in = bitarr![1, 0, 1, 0];
+            let _ = gate.run(&[], &[bad_in, good_in]);
+        } 
+
+        #[test]
+        #[should_panic]
+        fn input_validate_nand() {
+            let gate = Nand::new(4, 2);
+            // Should fail input validation
+            let bad_in = bitarr![1, 1, 1];
+            let good_in = bitarr![1, 0, 1, 0];
+            let _ = gate.run(&[], &[bad_in, good_in]);
+        } 
+
+        #[test]
+        #[should_panic]
+        fn input_validate_nor() {
+            let gate = Nor::new(4, 2);
+            // Should fail input validation
+            let bad_in = bitarr![1, 1, 1];
+            let good_in = bitarr![1, 0, 1, 0];
+            let _ = gate.run(&[], &[bad_in, good_in]);
+        } 
+
+        #[test]
+        #[should_panic]
+        fn input_validate_xnor() {
+            let gate = Xnor::new(4, 2);
+            // Should fail input validation
+            let bad_in = bitarr![1, 1, 1];
+            let good_in = bitarr![1, 0, 1, 0];
+            let _ = gate.run(&[], &[bad_in, good_in]);
+        } 
+
+        #[test]
+        #[should_panic]
+        fn input_validate_not() {
+            let gate = Not::new(4);
+            // Should fail input validation
+            let bad_in = bitarr![1, 1, 1];
+            let _ = gate.run(&[], &[bad_in]);
+        } 
+
+        #[test]
+        #[should_panic]
+        fn input_validate_tristate() {
+            let gate = TriState::new(4);
+            // Should fail input validation
+            let bad_in = bitarr![1, 1, 1];
+            let _ = gate.run(&[], &[bad_in]);
+        } 
+    }
+}
---
+++ src/node/wiring.rs
@@ -0,0 +1,212 @@
+use crate::bitarray::BitArray;
+use crate::node::{Component, PortProperties, PortType, PortUpdate, Sensitivity, port_list};
+
+/// An input.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Input {
+    bitsize: u8
+}
+impl Input {
+    /// Creates a new instance of the tri-state buffer with specified bitsize.
+    pub fn new(bitsize: u8) -> Self {
+        Self {
+            bitsize: bitsize.clamp(BitArray::MIN_BITSIZE, BitArray::MAX_BITSIZE)
+        }
+    }
+}
+impl Component for Input {
+    fn ports(&self) -> Vec<PortProperties> {
+        port_list(&[
+            // output
+            (PortProperties { ty: PortType::Output, bitsize: self.bitsize }, 1),
+        ])
+    }
+
+    fn run_inner(&self, _old_inp: &[BitArray], _inp: &[BitArray]) -> Vec<PortUpdate> {
+        vec![]
+    }
+}
+
+/// An output.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Output {
+    bitsize: u8
+}
+impl Output {
+    /// Creates a new instance of the tri-state buffer with specified bitsize.
+    pub fn new(bitsize: u8) -> Self {
+        Self {
+            bitsize: bitsize.clamp(BitArray::MIN_BITSIZE, BitArray::MAX_BITSIZE)
+        }
+    }
+}
+impl Component for Output {
+    fn ports(&self) -> Vec<PortProperties> {
+        port_list(&[
+            // output
+            (PortProperties { ty: PortType::Input, bitsize: self.bitsize }, 1),
+        ])
+    }
+
+    fn run_inner(&self, _old_inp: &[BitArray], _inp: &[BitArray]) -> Vec<PortUpdate> {
+        vec![]
+    }
+}
+
+/// A constant.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Constant {
+    value: BitArray
+}
+impl Constant {
+    /// Creates a new instance of the tri-state buffer with specified bitsize.
+    pub fn new(value: BitArray) -> Self {
+        Self { value }
+    }
+}
+impl Component for Constant {
+    fn ports(&self) -> Vec<PortProperties> {
+        port_list(&[
+            // output
+            (PortProperties { ty: PortType::Output, bitsize: self.value.len() }, 1),
+        ])
+    }
+
+    fn initialize(&self, state: &mut [BitArray]) {
+        state[0] = self.value;
+    }
+    fn run_inner(&self, _old_inp: &[BitArray], _inp: &[BitArray]) -> Vec<PortUpdate> {
+        vec![]
+    }
+}
+
+/// A splitter component.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Splitter {
+    bitsize: u8
+}
+impl Splitter {
+    /// Creates a new instance of the Splitter with specified bitsize.
+    pub fn new(bitsize: u8) -> Self {
+        Self {
+            bitsize: bitsize.clamp(BitArray::MIN_BITSIZE, BitArray::MAX_BITSIZE)
+        }
+    }
+}
+impl Component for Splitter {
+    fn ports(&self) -> Vec<PortProperties> {
+        port_list(&[
+            // joined
+            (PortProperties { ty: PortType::Inout, bitsize: self.bitsize }, 1),
+            // split
+            (PortProperties { ty: PortType::Inout, bitsize: 1 }, self.bitsize),
+        ])
+    }
+
+    fn run_inner(&self, old_ports: &[BitArray], new_ports: &[BitArray]) -> Vec<PortUpdate> {
+        if Sensitivity::Anyedge.activated(old_ports[0], new_ports[0]) {
+            std::iter::zip(1.., new_ports[0])
+                .map(|(index, bit)| PortUpdate { index, value: BitArray::from(bit) })
+                .collect()
+        } else if Sensitivity::Anyedge.any_activated(&old_ports[1..], &new_ports[1..]) {
+            let value = new_ports[1..].iter()
+                .map(|b| b.index(0))
+                .collect();
+            vec![PortUpdate { index: 0, value }]
+        } else {
+            vec![]
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{bitarray::{BitArray, BitState}, node::floating_ports};
+
+    #[test]
+    fn test_splitter_split() {
+        for bitsize in BitArray::MIN_BITSIZE..=BitArray::MAX_BITSIZE {
+            let splitter = Splitter::new(bitsize);
+            let props = splitter.ports();
+
+            assert_eq!(props.len(), 1 + bitsize as usize, "Splitter with bitsize {bitsize} should have {} ports", 1 + bitsize as usize);
+            assert_eq!(props[0], PortProperties { ty: PortType::Inout, bitsize }, "First Splitter port should be an inout of bitsize {bitsize}");
+            assert_eq!(
+                props[1..],
+                vec![PortProperties { ty: PortType::Inout, bitsize: 1 }; bitsize as usize],
+                "Splitter with bitsize {bitsize} should have {bitsize} split ports"
+            );
+
+            let old_ports = floating_ports(&props);
+            let mut new_ports = floating_ports(&props);
+            
+            let data: Vec<_> = (0..bitsize)
+                .map(|i| match i % 2 == 0 {
+                    true => BitState::High,
+                    false => BitState::Low
+                }).collect();
+            
+            let joined = BitArray::from_iter(data.iter().copied());
+            let result = new_ports[0].replace(joined);
+            assert!(result.is_ok());
+            
+            let actual = splitter.run(&old_ports, &new_ports);
+            let expected: Vec<_> = data.iter().enumerate()
+                .map(|(i, &st)| PortUpdate {
+                    index: 1 + i,
+                    value: BitArray::from(st)
+                })
+                .collect();
+
+            assert_eq!(
+                actual,
+                expected,
+                "Splitter should correctly split the input bits"
+            );
+        }
+    }
+
+    #[test]
+    fn test_splitter_join() {
+        for bitsize in BitArray::MIN_BITSIZE..=BitArray::MAX_BITSIZE {
+            let splitter = Splitter::new(bitsize);
+            let props = splitter.ports();
+
+            assert_eq!(props.len(), 1 + bitsize as usize, "Splitter with bitsize {bitsize} should have {} ports", 1 + bitsize as usize);
+            assert_eq!(props[0], PortProperties { ty: PortType::Inout, bitsize }, "First Splitter port should be an inout of bitsize {bitsize}");
+            assert_eq!(
+                props[1..],
+                vec![PortProperties { ty: PortType::Inout, bitsize: 1 }; bitsize as usize],
+                "Splitter with bitsize {bitsize} should have {bitsize} split ports"
+            );
+
+            let old_ports = floating_ports(&props);
+            let mut new_ports = floating_ports(&props);
+            
+            let data: Vec<_> = (0..bitsize)
+                .map(|i| match i % 2 == 0 {
+                    true => BitState::High,
+                    false => BitState::Low
+                }).collect();
+            
+            let joined = BitArray::from_iter(data.iter().copied());
+            let split: Vec<_> = data.into_iter()
+                .map(BitArray::from)
+                .collect();
+            for (p, arr) in std::iter::zip(new_ports[1..].iter_mut(), split) {
+                let result = p.replace(arr);
+                assert!(result.is_ok());
+            }
+            
+            let actual = splitter.run(&old_ports, &new_ports);
+            let expected = vec![PortUpdate { index: 0, value: joined }];
+
+            assert_eq!(
+                actual,
+                expected,
+                "Splitter should correctly split the input bits"
+            );
+        }
+    }
+}
